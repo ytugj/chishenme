e5a0765e0fe2273c0c6d0dd7c88a45356871164d
包子甘蓝 <45753749+ytugj@users.noreply.github.com>
2026-05-14 11:19:52 +0800

    Add files via upload

---
+++ three_punch_combo.js
@@ -0,0 +1,16 @@
+$i = 0;
+$("#start").click(function () {
+  $i++;
+  if ($i >= 6) {
+    $("#start").hide();
+    $("#stop").show();
+  }
+});
+$("#stop").click(function () {
+  alert("这么作？今天别吃了！");
+  $(this).hide();
+  // $("#banner").show();
+});
+// $("#close_banner").click(function () {
+//   $("#banner").hide();
+// });
